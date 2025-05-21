86fdb23df3ed8c76a612a1e796afdd02dbfd5054
Dean Fox <deanfoxcd@gmail.com>
2025-05-20 21:01:42 -0400

    Added newsletter section and styled homepage

---
+++ src/app/_components/newsletter.tsx
@@ -0,0 +1,82 @@
+import { Button } from 'flowbite-react';
+import { customTheme } from '../styles/themes';
+import '../globals.css';
+
+const inputStyle =
+  'bg-white text-black p-3 border-1 border-[var(--main-brown)] rounded-lg focus:border-2 focus:border-[var(--main-brown)] focus:ring-1 focus:ring-[var(--main-brown)] outline-none';
+
+const Newsletter: React.FC = function () {
+  return (
+    <section className='bg-transparent text-white mb-20'>
+      <div className='mx-auto max-w-screen-xl lg:py-8 lg:px-2'>
+        <div className='mx-auto max-w-screen-md sm:text-center'>
+          <h2 className='mb-6 text-3xl tracking-tight font-extrabold sm:text-4xl '>
+            Join the studio newsletter
+          </h2>
+          <p className='mx-auto mb-2 max-w-2xl font-light md:mb-4 sm:text-xl '>
+            Be the first to know when a collection is launched, be notified when
+            a spot opens up on the commissions waiting list, and learn more
+            about the behind the scenes in the studio!
+          </p>
+          <form action='#'>
+            <div className='items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex-col sm:space-y-0'>
+              <div className='flex gap-4 p-4'>
+                <label
+                  htmlFor='firstName'
+                  className='hidden'
+                >
+                  First Name
+                </label>
+                <input
+                  type='text'
+                  id='firstName'
+                  placeholder='First Name'
+                  className={[inputStyle, 'w-full'].join(' ')}
+                />
+                <label
+                  htmlFor='lastName'
+                  className='hidden'
+                >
+                  Last Name
+                </label>
+                <input
+                  type='text'
+                  id='lastName'
+                  placeholder='Last Name'
+                  className={[inputStyle, 'w-full'].join(' ')}
+                />
+              </div>
+              <div className='relative w-full'>
+                <label
+                  htmlFor='email'
+                  className='hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+                >
+                  Email address
+                </label>
+
+                <input
+                  className={[inputStyle, 'w-auto'].join(' ')}
+                  placeholder='Email address'
+                  type='email'
+                  id='email'
+                  required
+                />
+              </div>
+              <div className='flex justify-center items-center mt-4'>
+                <Button
+                  theme={customTheme.button}
+                  color='alternative'
+                  pill
+                >
+                  Join the club
+                </Button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Newsletter;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+
 import './globals.css';
 import Header from './_components/header';
 import Navbar from './_components/navbar';
 import { FooterComponent } from './_components/footer';
 import { ThemeModeScript } from 'flowbite-react';
 import BackgroundImage from './_components/backgroundImg';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Sable Fox Studio',
@@ -35,13 +25,13 @@ export default function RootLayout({
       <head>
         <ThemeModeScript />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <BackgroundImage />
-        <Header />
-        <Navbar />
-        <main className='mt-12'>{children}</main>
+      <body className='antialiased'>
+        <div>
+          <BackgroundImage />
+          <Header />
+          <Navbar />
+        </div>
+        <main>{children}</main>
         <FooterComponent />
       </body>
     </html>
