0cc9c558ea3bcf7b70b47b71eadc64fe41d60b9d
Dean Fox <deanfoxcd@gmail.com>
2025-06-09 18:22:49 -0400

    Added quantities to cart items and switched to Next Image in the product cards

--- src/app/_components/productCard.tsx
+++ src/app/_components/productCard.tsx
@@ -1,55 +1,61 @@
 import { Card } from 'flowbite-react';
 import { Product } from '../types';
 import ButtonProducts from './buttonProducts';
+import QuantityPicker from './quantityPicker';
+import Image from 'next/image';
 
 interface ProductsCardProps {
   product: Product;
   role: string;
   onEdit?: () => void;
+  quantity?: number;
 }
 
 const ProductCard: React.FC<ProductsCardProps> = function ({
   product,
   role,
   onEdit,
+  quantity,
 }) {
   return (
-    <Card
-      className='w-100'
-      imgAlt={product.name}
-      imgSrc=''
-    >
-      <a href='#'>
-        <h5 className='text-xl font-semibold tracking-tight text-white'>
-          {product.name}
-        </h5>
-      </a>
+    <div className='text-black dark:text-white'>
+      <Card className='w-100'>
+        <div className='flex items-center justify-center'>
+          <Image
+            src='/images/dog.jpeg'
+            alt='dog'
+            width={200}
+            height={200}
+          />
+        </div>
+        <h5 className='text-xl font-semibold tracking-tight'>{product.name}</h5>
 
-      <div>
-        <p className='text-lg text-black dark:text-white'>
-          {product.description}
-        </p>
-      </div>
+        <div>
+          <p className='text-lg '>{product.description}</p>
+        </div>
 
-      <div className='flex items-center justify-between'>
-        <span className='text-3xl font-bold text-gray-900 dark:text-white'>
-          ${product.price}
-        </span>
-        <ButtonProducts
-          role={role}
-          productId={product.id}
-          onEdit={onEdit}
-        >
-          {role === 'admin'
-            ? 'Edit product'
-            : role === 'shop'
-              ? 'Add to cart'
-              : role === 'cart'
-                ? 'Remove from cart'
-                : ''}
-        </ButtonProducts>
-      </div>
-    </Card>
+        <div className='flex items-center justify-between'>
+          <span className='text-3xl font-bold text-gray-900 dark:text-white'>
+            ${product.price}
+          </span>
+
+          <ButtonProducts
+            role={role}
+            productId={product.id}
+            onEdit={onEdit}
+          >
+            {role === 'admin'
+              ? 'Edit product'
+              : role === 'shop'
+                ? 'Add to cart'
+                : role === 'cart'
+                  ? 'Remove from cart'
+                  : ''}
+          </ButtonProducts>
+        </div>
+        {quantity && <QuantityPicker quantity={quantity} />}
+      </Card>
+    </div>
   );
 };
 
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,13 +2,14 @@
 
 import { useEffect, useState } from 'react';
 import ProductCard from '../_components/productCard';
-import { getCart } from '../_lib/cartActions';
+import { getCart, updateCartItem } from '../_lib/cartActions';
 import { getGuestId } from '../_lib/guestId';
 import { Product } from '../types';
 
 const Cart: React.FC = () => {
   const [guestId, setGuestId] = useState<string | null>(null);
   const [cart, setCart] = useState<Product[]>([]);
+  const [quantities, setQuantities] = useState<Record<string, number>>({});
 
   useEffect(() => {
     const id = getGuestId();
@@ -16,18 +17,31 @@ const Cart: React.FC = () => {
 
     getCart(id).then((cartData) => {
       if (cartData) {
+        console.log('Cart Data:', cartData);
+
+        const newQuantities: Record<string, number> = {};
+        cartData.forEach((product) => {
+          if (product.id) {
+            newQuantities[product.id] = (newQuantities[product.id] || 0) + 1;
+          }
+        });
+        setQuantities(newQuantities);
+
         const uniqueProducts = cartData.filter(
           (prod, i, self) => i === self.findIndex((p) => p.id === prod.id)
         );
         setCart(uniqueProducts);
+        console.log('Cart:', uniqueProducts);
       }
-      console.log('Cart:', cartData);
     });
 
     console.log('Guest id:', id);
   }, []);
 
-  console.log('CartItems:', cart);
+  // const updateQuantity = (productId: string, quantity: number) => {
+  //   if (!guestId) return;
+  //   updateCartItem(guestId, productId, quantity);
+  // };
 
   return (
     <div className='text-white min-h-screen'>
@@ -40,6 +54,7 @@ const Cart: React.FC = () => {
             <ProductCard
               product={product}
               role='cart'
+              quantity={quantities[product.id as string] || 0}
             />
           </li>
         ))}
