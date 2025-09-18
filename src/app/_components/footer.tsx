24dc2f3acd5e6187dc87a609746aef7bc3e9bb92
Dean Fox <deanfoxcd@gmail.com>
2025-09-17 21:39:48 -0400

    Added social media hrefs and made footer logo open /login

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -6,6 +6,7 @@ import {
   FooterCopyright,
   FooterIcon,
 } from 'flowbite-react';
+import Link from 'next/link';
 import {
   BsEnvelope,
   BsFacebook,
@@ -19,31 +20,36 @@ export const FooterComponent: React.FC = function () {
       <div className='w-full bg-[#E5E0DA]'>
         <div className='grid w-full justify-center sm:flex sm:justify-center md:flex md:grid-cols-1'>
           <div>
-            <FooterBrand
-              href=''
-              src='/images/Logo.png'
-              alt='Sable Fox Studio Logo'
-              name='Sable Fox Studio Logo'
-              className='h-70'
-            />
+            <Link
+              href='/login'
+              className='hover:cursor-default'
+            >
+              <FooterBrand
+                // href='/login'
+                src='/images/Logo.png'
+                alt='Sable Fox Studio Logo'
+                // name='Sable Fox Studio Logo'
+                className='h-70'
+              />
+            </Link>
           </div>
         </div>
         <div className='w-full sm:flex-col sm:items-center sm:justify-between'>
           <div className='my-4 flex space-x-6 sm:mt-0 sm:justify-center'>
             <FooterIcon
-              href='#'
+              href='https://www.facebook.com/sablefoxstudio/'
               icon={BsFacebook}
             />
             <FooterIcon
-              href='#'
+              href='https://www.instagram.com/sablefoxstudio/'
               icon={BsInstagram}
             />
             <FooterIcon
-              href='#'
+              href='https://www.pinterest.com/sablefoxstudio/'
               icon={BsPinterest}
             />
             <FooterIcon
-              href='#'
+              href='mailto:sablefoxstudio@gmail.com'
               icon={BsEnvelope}
             />
           </div>
